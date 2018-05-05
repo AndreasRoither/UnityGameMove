e4524ab1d0b38c82663668f00b5b8e4bc83f6b1f
AndiRoither <andi.roither@hotmail.de>
2018-05-05 18:54:33 +0200

    [new] Camera, Materials, Scripts, UI
    Added:
    - Camera follow script
    - Player movement script
    - UI text
    - Score
    - Fog
    - Obstacle prefab
    - Roboto Font

---
+++ Move/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 100;
+
+    private bool moveLeft = false;
+    private bool moveRight = false;
+
+    // Fixed Update for physics
+	void FixedUpdate () {
+        // even out frames per second on a better pc with deltaTime
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (moveRight)
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            moveRight = false;
+        }
+            
+        if (moveLeft)
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            moveLeft = false;
+        }    
+    }
+
+    void Update()
+    {
+        // check for input
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+            moveRight = true;
+
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+            moveLeft = true;
+    }
+}
